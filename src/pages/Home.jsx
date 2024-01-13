043f256ff449f4029b933b1e656b7ea182b27472
devjunayed <devjunayed@gmail.com>
2024-01-13 20:28:12 +0600

    randomizer main functionality done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,11 @@
 import { useState } from "react";
 import ModalPopup from "../components/ModalPopup";
 import { CopyToClipboard } from 'react-copy-to-clipboard';
+import { MdContentCopy } from "react-icons/md";
 
 const Home = () => {
     const [modalIsOpen, setIsOpen] = useState(false);
-    const [value, setValue] = useState();
+    const [value, setValue] = useState("");
     const [randomValue, setRandomValue] = useState([]);
 
     function openModal() {
@@ -31,24 +32,46 @@ const Home = () => {
     }
 
     return (
-        <div className="mx-10">
+        <div className="mx-10 overflow-y-auto">
             <div className="flex justify-center mt-10">
                 <button onClick={openModal} className="btn bg-green-600 text-white">Enter Data</button>
             </div>
-            <p className="text-center mt-4">Entered Data: {value}</p>
+            {
+                value.length > 0 &&
+                <div className="bg-yellow-100 p-2 mt-6">
+                    <p className="text-center">{value}</p>
+                </div>
+            }
 
-            <div className="bg-orange-200 rounded-3xl py-6 px-20 w-1/2 mx-auto mt-6">
-                {
-                    randomValue.map((data, index) => <div key={index}>{data}</div>)
-                }
-            </div>
+            {
+                randomValue.length > 0 &&
 
-            <div className="flex justify-center mt-6">
-                <button onClick={handleRandomize} className="btn bg-green-600 text-white">Randomize</button>
-            </div>
-            {/* <CopyToClipboard text={value}>
-                <button>Copy to clipboard with button</button>
-            </CopyToClipboard> */}
+                <div className="drop-shadow-lg bg-orange-50 rounded-3xl p-6 w-1/2 mx-auto mt-6">
+                    <div className="flex justify-between">
+                        <div className="flex  gap-2 items-center">
+                            <label htmlFor="checkbox">Add Serial Number</label>
+                            <input  type="checkbox" id="checkbox" name="" />
+                        </div>
+                        <div className="flex">
+                            <CopyToClipboard text={randomValue}>
+                                <button className="text-2xl "><MdContentCopy /></button>
+                            </CopyToClipboard>
+                        </div>
+                    </div>
+                    <div className="divider"></div>
+                    {
+                        randomValue.map((data, index) => <div key={index}>{data}</div>)
+                    }
+                </div>
+            }
+
+            {
+                value.length > 0 &&
+                <div className="flex justify-center mt-6">
+                    <button onClick={handleRandomize} className="btn bg-green-600 text-white">Randomize</button>
+                </div>
+
+            }
 
             <ModalPopup
                 setIsOpen={setIsOpen}
